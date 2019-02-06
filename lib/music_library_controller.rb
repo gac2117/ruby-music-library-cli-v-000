427efeade0e2f1ead77cc2b841f36fd72a3d8751
Grace Choi <oiograceoio@gmail.com>
2019-02-06 03:46:25 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -43,6 +43,8 @@ class MusicLibraryController
       def list_artists
         Artist.all.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |a, i|
           puts "#{i}. #{a.name}"
+        end
+      end
     end
   end
 end
