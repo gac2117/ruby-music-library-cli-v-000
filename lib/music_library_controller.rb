bcef873d237c655e480a14407ea7d95acb37a204
Grace Choi <oiograceoio@gmail.com>
2019-02-06 01:40:21 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -4,7 +4,17 @@ class MusicLibraryController
   end
 
   def call
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
+    input = ""
+
+    while input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+
+      input = gets.strip
+
+      case input
+      when 'list songs'
+        list_songs
+      when 
   end
 end
