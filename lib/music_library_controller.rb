b17718466198251fb0325ffa0efc925387f26356
Grace Choi <oiograceoio@gmail.com>
2019-02-06 03:53:42 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -56,7 +56,9 @@ class MusicLibraryController
         artist = ""
         puts "Please enter the name of an artist:"
         artist = gets.strip
-
+        Song.artist.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |s, i|
+          puts "#{i}. #{s.name} - #{s.genre.name}"
+        end
       end
     end
   end
