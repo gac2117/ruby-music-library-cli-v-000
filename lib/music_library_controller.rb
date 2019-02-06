9576349bf58058509ee85e908387065442c2d28d
Grace Choi <oiograceoio@gmail.com>
2019-02-06 03:44:23 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -36,7 +36,7 @@ class MusicLibraryController
 
       def list_songs
         Song.all.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |s,i|
-          puts "#{s.name}"
+          puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
         end
       end
     end
